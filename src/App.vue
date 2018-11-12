5e899137e0378af6c985117c548d4cb4ff882fd9
kswr <ksawery.oleszynski@gmail.com>
2018-11-12 18:37:21 +0100

    starting tutorial from travishorn

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
+    <simple-map></simple-map>
   </div>
 </template>
 
 <script>
 
+import simpleMap from './components/SimpleMap'
+
 export default {
   components: {
+    'simple-map': simpleMap
   }
 }
 </script>
 
 <style>
+  @import "../node_modules/leaflet/dist/leaflet.css";
 
 </style>
---
+++ src/components/SimpleMap.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="mapid">
+
+  </div>
+</template>
+
+<script>
+// import Leaflet from 'leaflet'
+
+export default {
+  name: 'SimpleMap'
+}
+
+</script>
+
+<style scoped>
+  #mapid {
+    height: 180px;
+  }
+
+</style>
